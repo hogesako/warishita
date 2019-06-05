192c01ffe3d2c291fca8e0b154d54f6a853a723a
hogesako <hogesako@ikasako.com>
2019-06-05 21:46:53 +0900

    [add]add toppage

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get 'contents/news'
   get 'contents/ticket'
   get 'contents/:id', to: 'contents#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top#show'
 end
