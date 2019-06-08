ee34321ca8693a791619fc7872e1c3aa8cf13aaf
hogesako <hogesako@ikasako.com>
2019-06-08 10:36:04 +0900

    categoryの結合をleft outer joinに

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,11 +1,11 @@
 class ContentsController < ApplicationController
   def news
-    @contents = Content.joins(:category).page(params[:page]).order(updated_at: "DESC")
+    @contents = Content.eager_load(:category).page(params[:page]).order(updated_at: "DESC")
   end
   def ticket
 
   end
   def show
-    @content = Content.joins(:category).find(params[:id])
+    @content = Content.eager_load(:category).find(params[:id])
   end
 end
