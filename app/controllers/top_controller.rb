140e1f06b22ccb55df8758883add45e17f1ee121
hogesako <hogesako@ikasako.com>
2019-06-09 21:03:37 +0900

    top pageもphotos対応

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 class TopController < ApplicationController
   def show
     @contents = Content.eager_load(:category).select("*").limit(3).order(updated_at: "DESC")
+
+    #TODO Movieも検索できるようにする。union all or view?
+    @pages = Photo.eager_load(:photo_category, :img_size).select("*").limit(3).order(updated_at: "DESC")
   end
 end
