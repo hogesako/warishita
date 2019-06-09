39a2bf0213bf562a83cc8a0cde5ca5dd08d12181
hogesako <hogesako@ikasako.com>
2019-06-09 21:06:20 +0900

    whatsnewのページャーURL修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,19 @@ Rails.application.routes.draw do
   post 'users/signin', to: 'user_sessions#create'
   get 'signout', to: 'user_sessions#destroy'
   get 'signup', to: 'users#new'
-
   get 'dashboard', to: 'users#dashboard'
-
   resources :users
-  get 'movies/show'
+
+  get 'pages/update', to: 'pages#index',defaults: { page: '1' }
+  get 'pages/update/page/:page', to: 'pages#index'
+  get 'movies/:id', to: 'movies#show'
   get 'photos/:id', to: 'photos#show'
+
   get 'products', to: 'goods#show'
   get 'discography', to: 'discography#show'
   get 'profiles', to: 'profiles#show'
-  get 'pages/update', to: 'pages#index'
-  
+
+
   get 'contents/news', defaults: { page: '1' }
   get 'contents/news/page/:page', to: 'contents#news'
   get 'contents/ticket', defaults: { page: '1' }
